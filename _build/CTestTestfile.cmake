c0c8a45226f9e523697fa8c56a633a697dea5ab9
Toray-lab <net-vladislav@inbox.ru>
2026-06-09 23:30:25 +0300

    Add _build and _builds to .gitignore, remove build artifacts from tracking

--- _build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vlad/Toray-lab/workspace/projects/lab05
-# Build directory: /home/vlad/Toray-lab/workspace/projects/lab05/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/vlad/Toray-lab/workspace/projects/lab05/_build/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/vlad/Toray-lab/workspace/projects/lab05/CMakeLists.txt;31;add_test;/home/vlad/Toray-lab/workspace/projects/lab05/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
